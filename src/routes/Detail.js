c07a250fb47c8cb2a2b31ff6e7a0dd8385cf0e27
skystar234 <80614306+skystar234@users.noreply.github.com>
2021-08-13 23:19:47 +0900

    Add files via upload

---
+++ src/routes/Detail.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push("/");
+    }
+  }
+}
+
+export default Detail
